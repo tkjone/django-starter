d3e78c62016c2a483cb2d2ce6abb2ead51a73009
tkjone <thomasmattacchione@gmail.com>
2016-02-15 20:41:38 -0500

    SETTINGS FILES - STATIC SETTINGS
    Removed STATIC_ROOT from common.py
    Added a different setting for STATIC_DIRS in dev.py
    Added STATIC_ROOT to prod.py

--- {{cookiecutter.repo_name}}/config/settings/common.py
+++ {{cookiecutter.repo_name}}/config/settings/common.py
@@ -144,8 +144,6 @@ TEMPLATES = [
 # STATIC FILE CONFIGURATION
 # ------------------------------------------------------------------------------
 
-STATIC_ROOT = str(APPS_DIR('assets'))
-
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_URL = '/static/'
 
--- {{cookiecutter.repo_name}}/config/settings/dev.py
+++ {{cookiecutter.repo_name}}/config/settings/dev.py
@@ -27,6 +27,13 @@ TEMPLATES[0]['OPTIONS']['debug'] = DEBUG
 # Note: This key only used for development and testing.`
 SECRET_KEY = env("DJANGO_SECRET_KEY", default='CHANGEME!!!')
 
+# ------------------------------------------------------------------------------
+# STATIC
+# ------------------------------------------------------------------------------
+
+STATICFILES_DIRS = (
+    str(ROOT_DIR.path('assets')),
+)
 
 # ------------------------------------------------------------------------------
 # DJANGO DEBUG TOOLBAR
