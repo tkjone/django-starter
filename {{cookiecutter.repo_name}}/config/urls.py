706881daafe57beeb3c59a5add67053589884119
tkjone <thomasmattacchione@gmail.com>
2017-09-10 11:49:48 -0400

    issue-3 Configure django-toolbar

--- {{cookiecutter.repo_name}}/config/urls.py
+++ {{cookiecutter.repo_name}}/config/urls.py
@@ -4,6 +4,7 @@
 https://docs.djangoproject.com/en/1.8/topics/http/urls/
 
 """
+from django.conf import settings
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.views.generic import TemplateView
@@ -19,3 +20,10 @@ urlpatterns = [
         view=TemplateView.as_view(template_name='base.html'),
         name="home"),
 ]
+
+# url pattern for django debug toolbar
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += [
+        url(r'^__debug__/', include(debug_toolbar.urls)),
+    ]
